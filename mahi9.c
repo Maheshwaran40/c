32c89d2469df2229d8eb8cfba018cd21184bfa0c
Maheshwaran40 <35914702+Maheshwaran40@users.noreply.github.com>
2018-02-02 16:51:14 +0530

    Create mahi9.c

---
+++ mahi9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("Enter a positive integer: ");
+    scanf("%d", &i);
+    printf("Sum = %d",i);
+    return 0;
+}
+
+int i
+{
+    if(n != 0)
+        return 0;
+    else
+        return 0;
+}
