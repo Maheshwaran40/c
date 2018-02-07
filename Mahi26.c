99f4b36421c949fb46a99b31b87f8f6317beab7b
Maheshwaran40 <35914702+Maheshwaran40@users.noreply.github.com>
2018-02-07 15:08:00 +0530

    Update Mahi26.c

--- Mahi26.c
+++ Mahi26.c
@@ -1,33 +1,31 @@
-#include <stdio.h>
-typedef unsigned char boolean;
-#define True 't'
-#define FALSE (!True)
-void swap(long* a,long *b) { *a=*a^*b;*b=*b^*a;*a=*a^*b; }
-#define until(condition) while(!(condition))
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <time.h>
+#include<conio.h>
+std::string gen_random() {
+    size_t len = rand() % 25 + 5;
 
-int main(int n, char*args[]){
-  double *d;
-  int i;
-  char input[10]; 
-  boolean sorted = FALSE;
-  printf("Please enter the length of the array\n");
-  gets(input);
-  sscanf(input,"%s",&input[1]);
-  n=(long)atol(input);
-  d = calloc(sizeof(double),n); 
-  until (sorted) {
-     for (i=1;i<n;i++) {
-        printf("Please enter the %d%s array item\n",i,i==2?"st":"th");
-        scanf("%2f",d+i);
-     }
-     sorted = !sorted
-     printf("%d %d\n",i,n);
-     for (i=2;i<n;i++) {
-        if (d[i]<d[i-2]) {
-          swap(d+i,d+i-2);
-          sorted = FALSE;
-        }
-     }
-     printf("The array is%ssorted\n",sorted?" ":" not "); }
-  for (;n-->1;) printf("%lf\n",*d++);
-  }
+    std::string x;
+
+    std::generate_n(std::back_inserter(x), len, rand);
+    return x;
+}
+
+static const int num = 10000000;
+
+int main(){
+    std::vector<std::string> strings;
+
+    std::generate_n(std::back_inserter(strings), num, gen_random);
+
+    clock_t start = clock();
+    std::sort(strings.begin(), strings.end());
+    clock_t ticks = clock() - start;
+
+    std::cout << ticks / (double)CLOCKS_PER_SEC;
+
+    getch();
+}
