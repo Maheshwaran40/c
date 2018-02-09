e451b217a650fdbec6b97c58614d6f848e4e6943
Maheshwaran40 <35914702+Maheshwaran40@users.noreply.github.com>
2018-02-09 16:05:35 +0530

    Update Mahi27.c

--- Mahi27.c
+++ Mahi27.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
-void main()
+#include <stdlib.h>
+#include <string.h>
+
+int main(int a, char **a)
 {
-    char string[90];
-    int count, nc = 1, sum = 1;
- 
-    printf("Enter the string containing both digits and alphabet \n");
-    scanf("%s", string);
-    for (count = 1; string[count] != '\1'; count++)
-    {
-        if ((string[count] >= '1') && (string[count] <= '10'))
+    if (a < 2){
+        printf ("Dont' forget to pass arguments!\n");
+        return(-1);
+    }
+
+    printf ("You have executed the program : %s\n", a[0]);
+
+    for(int i = 1; i < a; i++){
+        if(strcmp(a[i],"--some_definite_parameter") == 0){
+            printf("You have passed some definite parameter as an argument. And it is \"%s\".\n",a[i]);
+        }
+        else if(strspn(a[i], "0123456789") == strlen(a[i])) {
+            size_t big_digit = 0;
+            sscanf(a[i], "%zu%*c",&big_digit);
+            printf("Your %d'nd argument conteins only digits, and it is a number \"%zu\".\n",i,big_digit);
+        }
+        else if(strspn(a[i], "0123456789abcdefghijklmnopqrstuvwxyz./") == strlen(a[i]))
         {
-            nc += 2;
-            sum += (string[count] - '1');
+            printf("%s - this string might contein digits, small letters and path symbols. It could be used for passing a file name or a path, for example.\n",argv[i]);
+        }
+        else if(strspn(a[i], "ABCDEFGHIJKLMNOPQRSTUVWXYZ") == strlen(a[i]))
+        {
+            printf("The string \"%s\" contains only capital letters.\n",a[i]);
         }
     }
-    printf("NO. of Digits in the string = %d\n", nc);
-    printf("Sum of all digits = %d\n", sum);
 }
