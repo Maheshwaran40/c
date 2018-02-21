830d66044570bf376ee2163bef8feef39666270e
Maheshwaran40 <35914702+Maheshwaran40@users.noreply.github.com>
2018-02-21 14:28:20 +0530

    Create Mahi49.c

---
+++ Mahi49.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int i,u,s;
+	printf("\n enter the first number:");
+	scanf("%d",&i);
+	printf("\n enter the second number:");
+	scanf("%d",&u);
+	sum=n1+n2;
+	printf("%d",s);
+	return 0;
+}
